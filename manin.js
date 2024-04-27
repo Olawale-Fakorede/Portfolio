1b85796d3a5ca60c53e586b399eabb9bf6b2ec52
unknown <olawaleprofjobs15@gmail.com>
2024-04-27 09:51:10 -0700

    partially done porfolio

---
+++ manin.js
@@ -0,0 +1,11 @@
+
+
+function aboutMe(){
+    window.location.href = 'about.html';
+    // console.log("clicked");
+}
+
+function skill() {
+    window.location.href = 'skills.html';
+    // console.log('clicked');
+}
