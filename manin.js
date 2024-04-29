465fb481140ea5452a067ea79e73e0d5a0d3c0ee
unknown <olawaleprofjobs15@gmail.com>
2024-04-29 12:27:12 -0700

    projects add.

--- manin.js
+++ manin.js
@@ -9,3 +9,41 @@ function skill() {
     window.location.href = 'skills.html';
     // console.log('clicked');
 }
+
+function myProjs() {
+    window.location.href = 'projects.html';
+    // console.log('clicked');
+}
+
+
+
+
+
+// ABOUT DOWNLOAD JS
+
+
+// Get the portfolio container and download button
+const portfolioContainer = document.getElementById("portfolio");
+const downloadButton = document.getElementById("download-button");
+
+// Add a PDF file to the portfolio
+// const pdfData =
+const pdfFile = new Blob([pdfData], { type: "resume/pdf" }); 
+// pdfData is the PDF file data
+const pdfUrl = URL.createObjectURL(pdfFile);
+const pdfLink = document.createElement("a");
+pdfLink.href = pdfUrl;
+pdfLink.download = "Olawale Fakorede Web Development Resume.pdf"; // set the download file name
+portfolioContainer.appendChild(pdfLink);
+
+// Add an event listener to the download button
+// downloadButton.addEventListener("click", () => {
+//   pdfLink.click(); // simulate a click on the PDF link to download the file
+
+//   console.log("clicked");
+// });
+
+function downLoad(){
+    pdfLink.click();
+    console.log('clicked');
+}
